9b0e4c6f074452cdf5f8bd345ec9f49e4bf1e33f
localadmin LPS <localadminlps@JMccormickMac.local>
2014-11-24 13:34:00 -0500

    none

--- HearMeNow/ViewController.m
+++ HearMeNow/ViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by localadmin LPS on 11/21/14.
 //  Copyright (c) 2014 John McCormick. All rights reserved.
-//
+//  This is the view controller implementation file
 
 #import "ViewController.h"
 
